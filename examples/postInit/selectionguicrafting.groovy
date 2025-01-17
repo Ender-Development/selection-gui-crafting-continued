2af5f4f027ade68d9c3cf7298d3c2aa35d529963
MasterEnderman <xxmr.endermanxx@gmail.com>
2025-01-17 03:10:43 +0100

    add examples

---
+++ examples/postInit/selectionguicrafting.groovy
@@ -0,0 +1,67 @@
+
+// Auto generated groovyscript example file
+// MODS_LOADED: selectionguicrafting
+
+log.info 'mod \'selectionguicrafting\' detected, running script'
+
+// groovyscript.wiki.selectionguicrafting.selection_category.title:
+// groovyscript.wiki.selectionguicrafting.selection_category.description.
+
+mods.selectionguicrafting.selection_category.removeByName('test_category')
+
+mods.selectionguicrafting.selection_category.categoryBuilder()
+    .name('test_category')
+    .displayName('Test Category')
+    .register()
+
+mods.selectionguicrafting.selection_category.categoryBuilder()
+    .name('another_category')
+    .displayName('Another Category')
+    .register()
+
+
+// groovyscript.wiki.selectionguicrafting.selection_pair.title:
+// groovyscript.wiki.selectionguicrafting.selection_pair.description.
+
+mods.selectionguicrafting.selection_pair.removeByCategory('also_a_test_category')
+mods.selectionguicrafting.selection_pair.removeByInput(item('minecraft:stone'))
+mods.selectionguicrafting.selection_pair.removeByTool(item('minecraft:stone_pickaxe'))
+// mods.selectionguicrafting.selection_pair.removeAll()
+
+mods.selectionguicrafting.selection_pair.pairBuilder()
+    .input(item('minecraft:dirt'),item('minecraft:cobblestone'))
+    .addTool(item('minecraft:stone_pickaxe'), 1.0f, 1.0f)
+    .category('test_category')
+    .register()
+
+mods.selectionguicrafting.selection_pair.pairBuilder()
+    .input(item('minecraft:wool'),item('minecraft:diamond'))
+    .addTool(item('minecraft:golden_pickaxe'), 1.0f, 1.0f)
+    .addTool(item('minecraft:iron_pickaxe'), 1.0, 1.0)
+    .category('another_category')
+    .register()
+
+
+// groovyscript.wiki.selectionguicrafting.selection_recipe.title:
+// groovyscript.wiki.selectionguicrafting.selection_recipe.description.
+
+mods.selectionguicrafting.selection_recipe.removeByOutput(item('minecraft:stone'))
+// mods.selectionguicrafting.selection_recipe.removeAll()
+
+mods.selectionguicrafting.selection_recipe.recipeBuilder()
+    .category('test_category')
+    .inputQuantity(1)
+    .durabilityUsage(1)
+    .time(0)
+    .output(item('minecraft:apple'))
+    .register()
+
+mods.selectionguicrafting.selection_recipe.recipeBuilder()
+    .category('another_category')
+    .inputQuantity(1)
+    .durabilityUsage(1)
+    .time(0)
+    .output(item('minecraft:clay'), item('minecraft:snowball'))
+    .register()
+
+
