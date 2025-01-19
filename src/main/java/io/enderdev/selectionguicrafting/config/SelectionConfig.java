5c7e12438ddbca5476b47f5c0cc68f4c41bc81cc
MasterEnderman <xxmr.endermanxx@gmail.com>
2025-01-19 21:33:45 +0100

    rewrote the config

--- src/main/java/io/enderdev/selectionguicrafting/config/SelectionConfig.java
+++ src/main/java/io/enderdev/selectionguicrafting/config/SelectionConfig.java
@@ -5,14 +5,24 @@ import io.enderdev.selectionguicrafting.Tags;
 import net.minecraftforge.common.config.Config;
 import net.minecraftforge.common.config.ConfigManager;
 import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Config(modid = Tags.MOD_ID)
+@Config(modid = Tags.MOD_ID, name = Tags.MOD_ID, category = Tags.MOD_ID)
 public class SelectionConfig {
 
-    @Config.Comment("Clientside config: Disables selection crafting GUIs from displaying the 'Close GUI' button, as you can use E and ESC to exit the GUI and the button could be clutter to some people.")
-    public static boolean disableCloseGUIbutton = false;
+    @Config.Name("Client")
+    @Config.LangKey("config.selectionguicrafting.client")
+    public static final Client CLIENT = new Client();
 
+    public static class Client
+    {
+        @Config.Name("Disable Close GUI Button")
+        @Config.Comment("Clientside config: Disables selection crafting GUIs from displaying the 'Close GUI' button, as you can use E and ESC to exit the GUI and the button could be clutter to some people.")
+        public boolean disableCloseGUIbutton = false;
+    }
+
+    @Mod.EventBusSubscriber(modid = Tags.MOD_ID)
     public static class ConfigHandler
     {
         @SubscribeEvent
