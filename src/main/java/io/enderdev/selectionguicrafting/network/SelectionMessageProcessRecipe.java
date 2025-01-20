cc5e8e2cd3cd27bce570c355130587080b77223f
MasterEnderman <xxmr.endermanxx@gmail.com>
2025-01-20 15:28:49 +0100

    rewrote the server message handling, when finishing a recipe

--- src/main/java/io/enderdev/selectionguicrafting/network/SelectionMessageProcessRecipe.java
+++ src/main/java/io/enderdev/selectionguicrafting/network/SelectionMessageProcessRecipe.java
@@ -1,7 +1,6 @@
 package io.enderdev.selectionguicrafting.network;
 
-import io.enderdev.selectionguicrafting.proxy.CommonProxy;
-import io.enderdev.selectionguicrafting.recipe.GuiSelectionRecipeCategory;
+import io.enderdev.selectionguicrafting.registry.*;
 import io.netty.buffer.ByteBuf;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
@@ -11,12 +10,12 @@ import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 
 import java.nio.charset.Charset;
-
-import static io.enderdev.selectionguicrafting.proxy.CommonProxy.*;
+import java.util.Random;
 
 public class SelectionMessageProcessRecipe implements IMessage {
 
-    public SelectionMessageProcessRecipe(){}
+    public SelectionMessageProcessRecipe() {
+    }
 
     private String recipeCategory;
     private int recipeIndex;
@@ -28,7 +27,8 @@ public class SelectionMessageProcessRecipe implements IMessage {
         this.playerName = playerName;
     }
 
-    @Override public void toBytes(ByteBuf buf) {
+    @Override
+    public void toBytes(ByteBuf buf) {
         buf.writeInt(recipeCategory.length());
         buf.writeCharSequence(recipeCategory, Charset.defaultCharset());
 
@@ -38,7 +38,8 @@ public class SelectionMessageProcessRecipe implements IMessage {
         buf.writeCharSequence(playerName, Charset.defaultCharset());
     }
 
-    @Override public void fromBytes(ByteBuf buf) {
+    @Override
+    public void fromBytes(ByteBuf buf) {
         int recipeCategoryLength = buf.readInt();
         recipeCategory = (String) buf.readCharSequence(recipeCategoryLength, Charset.defaultCharset());
 
@@ -49,75 +50,70 @@ public class SelectionMessageProcessRecipe implements IMessage {
     }
 
 
-
     public static class SelectionMessageProcessRecipeHandler implements IMessageHandler<SelectionMessageProcessRecipe, IMessage> {
 
         @Override
         public IMessage onMessage(SelectionMessageProcessRecipe message, MessageContext ctx) {
-
-            GuiSelectionRecipeCategory category = CommonProxy.recipeCategories.get(message.recipeCategory);
-
-            if(category == null)
-                return null;
-            if(message.recipeIndex >= category.recipes.size())
-                return null;
-
             EntityPlayer player = FMLCommonHandler.instance().getMinecraftServerInstance().getPlayerList().getPlayerByUsername(message.playerName);
-            if(player == null)
+            if (player == null) {
                 return null;
+            }
 
             ItemStack itemMainhand = player.getHeldItemMainhand();
             ItemStack stackOffhand = player.getHeldItemOffhand();
+            if (itemMainhand.isEmpty() || stackOffhand.isEmpty()) {
+                return null;
+            }
 
-            int toolIndex = 0;
-            for (ItemStack itemTool : getToolFromCategory(message.recipeCategory)) {
-                if (ItemStack.areItemStacksEqual(new ItemStack(itemTool.getItem(), 1, 0, itemTool.getTagCompound()), new ItemStack(itemMainhand.getItem(), 1, 0, itemMainhand.getTagCompound()))) {
-                    boolean validMeta = false;
-                    if (getPairFromCategory(message.recipeCategory).durabilityMultipliers.get(toolIndex) == Float.MAX_VALUE) {
-                        if (itemTool.getMetadata() == itemMainhand.getMetadata()) {
-                            validMeta = true;
-                        }
+            GsCategory category = GsRegistry.getCategory(message.recipeCategory);
+            if (category == null) {
+                return null;
+            }
+
+            GsRecipe recipe = GsRegistry.getValidRecipes(category, itemMainhand).get(message.recipeIndex);
+            if (recipe == null) {
+                return null;
+            }
+
+            if (!recipe.isInputValid(stackOffhand)) {
+                return null;
+            }
+
+            if (!recipe.isToolValid(itemMainhand)) {
+                return null;
+            }
+
+            GsTool tool = recipe.getTool(itemMainhand);
+            if (tool == null) {
+                return null;
+            }
+
+            // If we got here, the recipe is valid and can be processed
+            Random random = new Random();
+            recipe.getOutputs().forEach(output -> {
+                ItemStack stack = output.getItemStack().copy();
+                GsEnum.OutputType outputType = recipe.getOutputType() != null ? recipe.getOutputType() : category.getOutputType();
+                if (random.nextFloat() < output.getChance()) {
+                    if (recipe.getOutputType() == GsEnum.OutputType.DROP) {
+                        player.dropItem(stack, false, true);
                     } else {
-                        validMeta = true;
-                    }
-                    if (validMeta) {
-                        for (ItemStack itemInput : getItemFromCategory(message.recipeCategory)) {
-                            boolean valid = false;
-                            if (itemInput.getMetadata() == Short.MAX_VALUE) {
-                                if (ItemStack.areItemStacksEqual(new ItemStack(itemInput.getItem(), 1, Short.MAX_VALUE, itemInput.getTagCompound()), (new ItemStack(stackOffhand.getItem(), 1, Short.MAX_VALUE, stackOffhand.getTagCompound())))) {
-                                    valid = true;
-                                }
-                            } else {
-                                if (ItemStack.areItemStacksEqual(new ItemStack(itemInput.getItem(), 1, itemInput.getMetadata(), itemInput.getTagCompound()), new ItemStack(stackOffhand.getItem(), 1, stackOffhand.getMetadata(), stackOffhand.getTagCompound()))) {
-                                    valid = true;
-                                }
-                            }
-
-                            if (valid) {
-                                if (category.recipes.get(message.recipeIndex).inputQuantity <= player.getHeldItemOffhand().getCount()) {
-
-                                    // If all requirements are met, do the actual recipe
-                                    for (ItemStack itemStack : category.recipes.get(message.recipeIndex).outputs) {
-                                        //FMLCommonHandler.instance().getMinecraftServerInstance().getPlayerList().getPlayerByUsername(message.playerName).addItemStackToInventory(itemStack.copy());
-                                        FMLCommonHandler.instance().getMinecraftServerInstance().getPlayerList().getPlayerByUsername(message.playerName).dropItem(itemStack.copy(), false, true);
-                                    }
-                                    stackOffhand.shrink(category.recipes.get(message.recipeIndex).inputQuantity);
-
-                                    float toolDurabilityMultiplier = getPairFromCategory(message.recipeCategory).durabilityMultipliers.get(toolIndex);
-                                    if (toolDurabilityMultiplier == Float.MAX_VALUE) {
-                                        player.getHeldItemMainhand().shrink(1);
-                                    } else {
-                                        player.getHeldItemMainhand().damageItem((int) (category.recipes.get(message.recipeIndex).durabilityUsage * toolDurabilityMultiplier), player);
-                                    }
-
-                                    return null;
-                                }
-                            }
+                        if (!player.inventory.addItemStackToInventory(stack)) {
+                            player.dropItem(stack, false, true);
                         }
                     }
                 }
-                toolIndex++;
+            });
+
+            stackOffhand.shrink(recipe.getAmount());
+
+            if (itemMainhand.isItemStackDamageable()) {
+                int toolDamage = (int) (recipe.getDurability() * tool.getDamageMultiplier());
+                itemMainhand.damageItem(toolDamage, player);
+            } else {
+                itemMainhand.shrink(tool.getItemStack().getCount());
             }
+
+            player.addExperience(recipe.getXp());
 
             return null;
         }
