dc1ba3a1f80a8dddb8d77681bb451fdf1c38637f
MasterEnderman <xxmr.endermanxx@gmail.com>
2025-04-02 01:01:42 +0200

    small cleanup

--- src/main/java/io/enderdev/selectionguicrafting/SelectionGuiCrafting.java
+++ src/main/java/io/enderdev/selectionguicrafting/SelectionGuiCrafting.java
@@ -22,19 +22,16 @@ public class SelectionGuiCrafting {
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
-
         proxy.preInit(event);
     }
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
-
         proxy.init(event);
     }
 
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event) {
-
         proxy.postInit(event);
     }
 
