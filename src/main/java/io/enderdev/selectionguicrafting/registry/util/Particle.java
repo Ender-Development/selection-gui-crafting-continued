4d301b0726c8d86ef1dda1c86fdaec5c993cff74
MasterEnderman <xxmr.endermanxx@gmail.com>
2025-04-27 13:11:50 +0200

    alternative particle constructor

--- src/main/java/io/enderdev/selectionguicrafting/registry/util/Particle.java
+++ src/main/java/io/enderdev/selectionguicrafting/registry/util/Particle.java
@@ -13,6 +13,12 @@ public class Particle {
         this.particleSpeed = particleSpeed;
     }
 
+    public Particle(EnumParticleTypes particleType) {
+        this.particleType = particleType;
+        this.particleCount = 10;
+        this.particleSpeed = 0.1F;
+    }
+
     public EnumParticleTypes getType() {
         return particleType == null ? EnumParticleTypes.VILLAGER_HAPPY : particleType;
     }
